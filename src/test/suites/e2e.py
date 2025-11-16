ea03ae7c1a4403a9c79514913079a1d4a5b99513
Samiran Saha <samiran.s89@gmail.com>
2025-11-16 16:12:42 +0530

    pytest changes

--- src/test/suites/e2e.py
+++ src/test/suites/e2e.py
@@ -1,13 +1,17 @@
 from src.pages.loginPage import LoginPage
 from dotenv import load_dotenv
 import os
+import os
 
 load_dotenv()
 
 
 #@pytest.mark.nondestructive
 def test_e2e(driver, base_url="https://bstackdemo.com/"):
-    #staging = os.environ.get("LOCAL") 
-    base_url="https://bstackdemo.com/"
-    login = LoginPage(driver)
+    base_url = os.environ.get("CX_TEST_URL", base_url)
+    page_source = driver.page_source
+    print(f"Page Source Length: {len(page_source)}")
+    # Assert that the page source length > 100
+    assert len(page_source) > 100, "Page source length is not greater than 100!"
+    
     
