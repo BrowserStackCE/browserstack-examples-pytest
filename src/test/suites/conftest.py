825b4c751f581160fdec2e80c9445546578b8aee
Samiran Saha <samiran.s89@gmail.com>
2025-11-16 18:18:02 +0530

    pytest cleanup changes

--- src/test/suites/conftest.py
+++ src/test/suites/conftest.py
@@ -1,11 +1,14 @@
 import pytest
-from appium import webdriver
+from selenium import webdriver
 
 @pytest.fixture(scope='function')
-def setWebdriver(request, session_capabilities):
-    remoteURL = "https://hub.browserstack.com/wd/hub"
-    driver = webdriver.Remote(remoteURL, session_capabilities)
-    request.node._driver = driver
-    request.instance.driver = driver
+def setWebdriver(session_capabilities):
+    remote_url = "https://hub.browserstack.com/wd/hub"
+
+    driver = webdriver.Remote(
+        command_executor=remote_url,
+        options=webdriver.ChromeOptions().from_capabilities(session_capabilities)
+    )
+
     yield driver
     driver.quit()
